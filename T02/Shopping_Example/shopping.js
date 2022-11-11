e1573f071354b93ebd900f5c1e6ff3176353b525
Kaja Jaronska <kaja.jaronska@gmail.com>
2022-11-11 17:33:21 +0000

    Add T02/Shopping_Example folder

---
+++ T02/Shopping_Example/shopping.js
@@ -0,0 +1,13 @@
+// Code here for Compulsory Task 2
+
+// Requestint shopping items from the user
+const item1 = prompt("Please enter the first item of your shopping list:");
+const item2 = prompt("Please enter the second item of your shopping list:");
+const item3 = prompt("Please enter the third item of your shopping list:");
+
+// Printing user's shopping list 
+console.log("Your mighty shopping list:");
+console.log(item1);
+console.log(item2);
+console.log(item3);
+
