5dff702834e8c1dd43b5294db91239569ee05775
Kaja Jaronska <kaja.jaronska@gmail.com>
2022-11-11 18:39:48 +0000

    Find total, maximum value and minimum value from user's chosen 10 number array

---
+++ T12/Task_1/numberManipulation.js
@@ -0,0 +1,32 @@
+///////////////////////////////////////////////////////////
+// Creating an array to store user's 10 chosen numbers 
+let userNumbers = [];
+
+while(userNumbers.length < 10) {
+    let userNum = Number(prompt("Please choose 10 numbers:"));
+    userNumbers.push(userNum);
+}
+
+console.log(`These are your chosen numbers: ${userNumbers}.`);
+
+///////////////////////////////////////////////////////////
+// Finding total of all numbers and outputting it for the user
+let total = 0;
+
+userNumbers.forEach(num => {
+    total += num;
+})
+
+console.log(`Total of all your numbers is: ${total}.`);
+
+///////////////////////////////////////////////////////////
+// Finding index of the maximum value and logging it for the user using for loop
+let highest = Math.max(...userNumbers);
+
+console.log(`The highest number on your list is: ${highest} with an index of ${userNumbers.indexOf(highest)}.`);
+
+///////////////////////////////////////////////////////////
+// Finding index of the minimum value and logging it for the user
+let lowest = Math.min(...userNumbers);
+
+console.log(`The highest number on your list is: ${lowest} with an index of ${userNumbers.indexOf(lowest)}.`);
