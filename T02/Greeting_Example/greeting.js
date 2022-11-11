128974f8f1151d3f001b3b4a5c4a6ee2f640aca8
Kaja Jaronska <kaja.jaronska@gmail.com>
2022-11-11 17:32:33 +0000

    Add T02/Greeting_Example folder

---
+++ T02/Greeting_Example/greeting.js
@@ -0,0 +1,12 @@
+// Code here for Compulsory Task 1
+
+// Requesting user's first name and printing it
+let userName = prompt("Hello there! Please enter your name!");
+console.log(userName);
+
+// Requesting user's age and printing it
+let userAge = prompt("Now please enter your age.");
+console.log(userAge);
+
+console.log("It’s nice to meet you!");
+
