534f47feeb13e351c8a1d6a11f60b11e526b24fc
Kaja Jaronska <kaja.jaronska@gmail.com>
2022-11-16 15:49:38 +0000

    Add variables with prompt asking user's input

---
+++ T12/Task_3/calculator.js
@@ -0,0 +1,12 @@
+// Asking user to enter first number 
+let firstNum = Number(prompt("Hey! Please enter your first number! 🔢"))
+
+// Asking user to enter second number 
+let secondNum = Number(prompt("Hey! Please enter your second number! 🔢"))
+
+// Asking user to choose which calculation they would like to perform
+let operator = prompt(`Please chooose which operation you would like to perform:
+- add ➕ (type "add")
+- substract ➖ (type "substract")
+- multiply ❌ (type "multiply")
+- divide ➗ (type "divide")`)
