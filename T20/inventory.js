c6d5adc6d4b6253a732904160687f92c2608d996
Kaja Jaronska <kaja.jaronska@gmail.com>
2022-11-28 21:34:04 +0000

    Push every instance of Shoes to the array

--- T20/inventory.js
+++ T20/inventory.js
@@ -1,12 +1,20 @@
 // Creating the Shoes class
 class Shoes {
     constructor(name, productCode, quantity, value) {
+
         this.name = name;
         this.productCode = productCode;
         this.quantity = quantity;
         this.value = value;
+        // Automatically adding each instance to the array
+        shoesArr.push(this);
     }
+
 }
+
+// Creating an empty array
+let shoesArr = [];
+
 
 // Creating 5 instances of the Shoes class
 let shoe1 = new Shoes("loafers", "123V6", 124, "$50");
@@ -15,7 +23,6 @@ let shoe3 = new Shoes("flip-flops", "601G0", 50, "$29");
 let shoe4 = new Shoes("sneakers", "246A1", 90, "$80");
 let shoe5 = new Shoes("slippers", "335D6", 100, "$25");
 
-// Creating an empty array
-let shoesArr = [];
 
 console.log(Shoes);
+console.log(shoesArr);
