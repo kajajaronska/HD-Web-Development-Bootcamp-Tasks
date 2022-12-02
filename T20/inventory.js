2d92da6bab630637f1be9ad387d8a209f008f6fe
Kaja Jaronska <kaja.jaronska@gmail.com>
2022-12-02 17:27:45 +0000

    Update function searching for any shoe within the array to output the result in a readable manner

--- T20/inventory.js
+++ T20/inventory.js
@@ -1,33 +1,40 @@
 // Creating the Shoes class
 class Shoes {
-    constructor(name, productCode, quantity, value) {
-
-        this.name = name;
-        this.productCode = productCode;
-        this.quantity = quantity;
-        this.value = value;
-        // Automatically adding each instance to the array
-        shoesArr.push(this);
-    }
-
+  constructor(name, productCode, quantity, value) {
+    this.name = name;
+    this.productCode = productCode;
+    this.quantity = quantity;
+    this.value = value;
+    // Automatically adding each instance to the array
+    shoesArr.push(this);
+  }
 }
 
 // Creating an empty array
 let shoesArr = [];
 
-
 // Creating 5 instances of the Shoes class
-let shoe1 = new Shoes("loafers", "123V6", 124, "$50");
-let shoe2 = new Shoes("sneakers", "563F7", 78, "$87");
+let shoe1 = new Shoes("sneakers", "123V6", 124, "$50");
+let shoe2 = new Shoes("high-heels", "563F7", 78, "$87");
 let shoe3 = new Shoes("flip-flops", "601G0", 50, "$29");
 let shoe4 = new Shoes("sneakers", "246A1", 90, "$80");
 let shoe5 = new Shoes("slippers", "335D6", 100, "$25");
 
 // Method to search or any shoe within the array
-let findShoes = (arr,shoeName) => {
-    return arr.filter(el => el["name"] === shoeName);
-}
-console.log(findShoes(shoesArr,"sneakers"));
+let findShoes = (arr, shoeName) => {
+  // Filtering shoes array by name to find shoes chosen by the user
+  let found = arr.filter((el) => el["name"] === shoeName);
+
+  // Creating easy to read output by looping through found array
+  console.log(`Search keyword: ${shoeName}`);
+  return found.forEach((element) => {
+    console.log(
+    `***************************\nProduct code: ${element.productCode}\nQuantity: ${element.quantity}\nValue: ${element.value}
+    `);
+  });
+};
+
+console.log(findShoes(shoesArr, "sneakers"));
 
 // // Method to find a shoe with the lowest value per item
 // let lowestValue = shoesArr.reduce((previous,current) => {
@@ -41,4 +48,3 @@ console.log(findShoes(shoesArr,"sneakers"));
 // };
 
 // console.dir(shoesArr);
-
