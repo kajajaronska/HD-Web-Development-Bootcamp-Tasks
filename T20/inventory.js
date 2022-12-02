75d5d2ef6949303f04ea9c95975aa8f055194399
Kaja Jaronska <kaja.jaronska@gmail.com>
2022-12-02 18:22:16 +0000

    Create methods on the Shoes class to enable change of attributes

--- T20/inventory.js
+++ T20/inventory.js
@@ -8,6 +8,26 @@ class Shoes {
     // Automatically adding each instance to the array
     shoesArr.push(this);
   }
+
+  // Method setting new name
+  setNewName(newName) {
+    this.name = newName;
+  }
+
+  // Method setting new product code
+  setNewProductCode(newProductCode) {
+    this.productCode = newProductCode;
+  }
+
+  // Method setting new quantity code
+  setNewQuantity(newQuantity) {
+    this.quantity = newQuantity;
+  }
+
+  // Method setting new value code
+  setNewValue(newValue) {
+    this.value = newValue;
+  }
 }
 
 // Creating an empty array
@@ -29,45 +49,65 @@ let findShoes = (arr, shoeName) => {
   console.log(`Search keyword: ${shoeName}`);
   return found.forEach((element) => {
     console.log(
-    `***************************\nProduct code: ${element.productCode}\nQuantity: ${element.quantity}\nValue: ${element.value}
-    `);
+      `***************************\nProduct code: ${element.productCode}\nQuantity: ${element.quantity}\nValue: ${element.value}
+    `
+    );
   });
 };
 
 // Calling the findShoes function
-findShoes(shoesArr,"sneakers");
-
+findShoes(shoesArr, "sneakers");
 
 // // Method to find a shoe with the lowest value per item
 let lowestValue = (arr) => {
-// Using reduce method to boil down the array to the item with lowest value
- let lowest = arr.reduce((previous,current) => {
+  // Using reduce method to boil down the array to the item with lowest value
+  let lowest = arr.reduce((previous, current) => {
     // Using ternary operator
     return previous["value"] < current["value"] ? previous : current;
-});
+  });
 
-// // Creating easy to read output
-return console.log(
+  // // Creating easy to read output
+  return console.log(
     `Lowest value item\n***************************\nName: ${lowest.name}\nProduct code: ${lowest.productCode}\nQuantity: ${lowest.quantity}\nValue: ${lowest.value}
-    `);
-}
-    
+    `
+  );
+};
+
 // Calling the lowestValue function
 lowestValue(shoesArr);
 
 // // Method to find a shoe with the highest value per item
 let highestValue = (arr) => {
-    // Using reduce method to boil down the array to the item with highest value
-     let highest = arr.reduce((previous,current) => {
-        // Using ternary operator
-        return previous["value"] > current["value"] ? previous : current;
-    });
-    
-    // // Creating easy to read output
-    return console.log(
-        `Highest value item\n***************************\nName: ${highest.name}\nProduct code: ${highest.productCode}\nQuantity: ${highest.quantity}\nValue: ${highest.value}
-        `);
-    }
-        
-    // Calling the highestValue function
-    highestValue(shoesArr);
+  // Using reduce method to boil down the array to the item with highest value
+  let highest = arr.reduce((previous, current) => {
+    // Using ternary operator
+    return previous["value"] > current["value"] ? previous : current;
+  });
+
+  // // Creating easy to read output
+  return console.log(
+    `Highest value item\n***************************\nName: ${highest.name}\nProduct code: ${highest.productCode}\nQuantity: ${highest.quantity}\nValue: ${highest.value}
+        `
+  );
+};
+
+// Calling the highestValue function
+highestValue(shoesArr);
+
+// Checking methods on the Shoes class
+console.log(shoe1)
+shoe1.setNewName("golf shoes");
+console.log(shoe1);
+
+console.log(shoe2)
+shoe2.setNewProductCode("113X7");
+console.log(shoe2);
+
+console.log(shoe3)
+shoe3.setNewQuantity(99);
+console.log(shoe3);
+
+console.log(shoe4)
+shoe4.setNewValue("$15");
+console.log(shoe4);
+
