6ad16aa9fa1ec4198a8cec60d651684ee964b4a4
Kaja Jaronska <kaja.jaronska@gmail.com>
2022-12-02 17:13:07 +0000

    Create a function to search for any shoe within an array

--- T20/inventory.js
+++ T20/inventory.js
@@ -18,11 +18,27 @@ let shoesArr = [];
 
 // Creating 5 instances of the Shoes class
 let shoe1 = new Shoes("loafers", "123V6", 124, "$50");
-let shoe2 = new Shoes("boots", "563F7", 78, "$87");
+let shoe2 = new Shoes("sneakers", "563F7", 78, "$87");
 let shoe3 = new Shoes("flip-flops", "601G0", 50, "$29");
 let shoe4 = new Shoes("sneakers", "246A1", 90, "$80");
 let shoe5 = new Shoes("slippers", "335D6", 100, "$25");
 
+// Method to search or any shoe within the array
+let findShoes = (arr,shoeName) => {
+    return arr.filter(el => el["name"] === shoeName);
+}
+console.log(findShoes(shoesArr,"sneakers"));
 
-console.log(Shoes);
-console.log(shoesArr);
+// // Method to find a shoe with the lowest value per item
+// let lowestValue = shoesArr.reduce((previous,current) => {
+//     return previous["value"] < current["value"] ? previous : current;
+// });
+
+// console.log(lowestValue);
+
+// for(property in lowestValue) {
+//     console.log(`${property}: ${lowestValue[property]}`);
+// };
+
+// console.dir(shoesArr);
+
