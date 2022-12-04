07c07bf339f0d933d3e5fd1a677c66a1e0676f52
Kaja Jaronska <kaja.jaronska@gmail.com>
2022-12-04 18:36:18 +0000

    Add Subjects subclass

--- T22/courses.js
+++ T22/courses.js
@@ -1,5 +1,6 @@
 // Creating class Courses 
 class Courses {
+
     constructor(courseName, contactWebsite){
         this.courseName = courseName;
         this.contactWebsite = contactWebsite;
@@ -11,3 +12,19 @@ class Courses {
     }
 
 }
+
+// Creatings subclass Subjects from Courses class
+class Subjects extends Courses {
+
+    constructor(courseName, contactWebsite, price, length, studentsPerClass) {
+        // super() method
+        super(courseName, contactWebsite);
+
+        this.price = price;
+        this.length = length;
+        this.studentsPerClass = studentsPerClass;
+    }
+}
+
+// cookery pastry, foodprepr, vegan dishes,
+// attributes: price, length (weeks), main_techaer, numperclass 
