8e04b2fc7a3b10b70bf2e6d287087f1a922b17c5
Kaja Jaronska <kaja.jaronska@gmail.com>
2022-12-04 19:45:45 +0000

    Add method to the SalariedEmployee class to calculate their monthly payout. Test on two instances.

--- T23/manage.js
+++ T23/manage.js
@@ -1,10 +1,10 @@
 // Creating a super class for Employee
 class Employee {
 
-    constructor(name, currency, salary) {
+    constructor(name, currency, baseSalary) {
         this.name = name;
         this.currency = currency;
-        this.salary = salary;
+        this.baseSalary = baseSalary;
     }
 
 }
@@ -14,12 +14,26 @@ class Employee {
 // 1. Salaried employees
 class SalariedEmployee extends Employee{
 
-    constructor(name, currency, salary,) {
-        super(name,currency,salary)
+    constructor(name, currency, baseSalary) {
+        super(name,currency,baseSalary)
     }
 
     // Adding a constant attribute to the class
     typeOfEmployment = "salary";
+
+    // Method to calculate month's salary taking this month's target and sales as arguments
+    calcPayout(target,sales){
+        let payout,description;
+        if(sales > target) {
+           payout = this.baseSalary * 1.1;
+           description = `${this.name} hit ${this.currency}${sales} sales this month, which was higher than their target of ${this.currency}${target}.\nTherefore their base salary of ${this.currency}${this.baseSalary} has been increased by 10% 🎉`
+        } else {
+            payout = this.baseSalary;
+            description = description = `${this.name} made ${this.currency}${sales} sales this month, which was not higher than their target of ${this.currency}${target}.\nTherefore their base salary of ${this.currency}${this.baseSalary} remains unchanged.`;
+        }
+
+        return console.log(`Name: ${this.name}\nEmployee type: ${this.typeOfEmployment}\nBase salary: ${this.currency}${this.baseSalary}\nTarget: ${this.currency}${target}\nSales: ${this.currency}${sales}\nPayout:${this.currency}${payout}\nCalculations explained: ${description}`)
+    }
 }
 
 // 2. Hourly employees
@@ -44,3 +58,11 @@ class HybridEmployee extends Employee{
     typeOfEmployment = "hybrid";
 };
 
+// Creating Salaried Employees instances
+let paulSmith = new SalariedEmployee("Paul Smith", "£", 2000);
+let meganDolly = new SalariedEmployee("Megan Dolly", "£", 2200);
+
+console.log(paulSmith);
+
+paulSmith.calcPayout(30000,20000);
+meganDolly.calcPayout(10000,100000);
