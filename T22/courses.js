ff8ba6936f338c63a3a120f526643d6e0af2cd88
Kaja Jaronska <kaja.jaronska@gmail.com>
2022-12-04 18:51:51 +0000

    Add three instances of Subjects class and test superclass and subclass methods

--- T22/courses.js
+++ T22/courses.js
@@ -1,35 +1,49 @@
-// Creating class Courses 
+// Creating class Courses
 class Courses {
+  constructor(courseName, contactWebsite) {
+    this.courseName = courseName;
+    this.contactWebsite = contactWebsite;
+  }
 
-    constructor(courseName, contactWebsite){
-        this.courseName = courseName;
-        this.contactWebsite = contactWebsite;
-    }
-
-    // Method to display contact website
-    displayWebsite() {
-        console.log(this.contactWebsite);
-    }
-
+  // Method to display contact website
+  displayWebsite() {
+    console.log(this.contactWebsite);
+  }
 }
 
 // Creatings subclass Subjects from Courses class
 class Subjects extends Courses {
+  constructor(
+    courseName,
+    contactWebsite,
+    subjectName,
+    price,
+    numOfClasses,
+    studentsPerClass
+  ) {
+    // super() method
+    super(courseName, contactWebsite);
 
-    constructor(courseName, contactWebsite, subjectName, price, length, studentsPerClass) {
-        // super() method
-        super(courseName, contactWebsite);
+    this.subjectName = subjectName;
+    this.price = price;
+    this.numOfClasses = numOfClasses;
+    this.studentsPerClass = studentsPerClass;
+  }
 
-        this.subjectName = subjectName;
-        this.price = price;
-        this.length = length;
-        this.studentsPerClass = studentsPerClass;
-    }
-
-    displayCourseDetails() {
-        console.log(`Subject ${this.subjectName} belongs to course ${this.courseName}.\nIt will last for ${this.length} weeks and a price tag is ${this.price}.\nYou can find more information on ${this.contactWebsite}`)
-    }
+  displayCourseDetails() {
+    console.log(
+      `Subject ${this.subjectName} belongs to the ${this.courseName} course.\nThere will be ${this.numOfClasses} classes in total with ${this.studentsPerClass} students per class and the price tag is ${this.price}.\nYou can find more information on ${this.contactWebsite}.`
+    );
+  }
 }
 
-// cookery pastry, foodprepr, vegan dishes,
-// attributes: price, length (weeks), main_techaer, numperclass 
+// Creating 3 instances of Subjects class
+
+let pastryMaking = new Subjects("Culinary Arts", "www.cookery-school.com","Pastry Making", "£100", 5, 10);
+let foodPrep = new Subjects("Culinary Arts", "www.cookery-school.com","Food Preparation","£99", 5, 12);
+let veganDishes = new Subjects("Culinary Arts", "www.cookery-school.com","Vegan Dishes","£120", 10, 20);
+
+// Testing methods
+pastryMaking.displayCourseDetails();
+foodPrep.displayCourseDetails();
+veganDishes.displayWebsite();
