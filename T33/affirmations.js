a081f2d21e93ee0aa17c3a1e87e7657a17fafcaf
Kaja Jaronska <kaja.jaronska@gmail.com>
2023-01-02 21:10:49 +0000

    Add comments to explain the code

--- T33/affirmations.js
+++ T33/affirmations.js
@@ -1,10 +1,11 @@
-// Create API call
+// create API call
 
 fetch("https://www.affirmations.dev/")
-    .then(res => res.json())
+    .then(res => res.json()) // convert sent text into an object usong JSON
     .then((result)=> {
-        console.log(result.affirmation)
+        console.log(result.affirmation) // access value of the affirmation key
     }),
+    // error handling
     (error) => {
         console.log(error);
     }
