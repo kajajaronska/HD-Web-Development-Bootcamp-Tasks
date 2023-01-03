5f6bac4726a269c0b6b78dcbc8c4273f865bb978
Kaja Jaronska <kaja.jaronska@gmail.com>
2023-01-03 22:07:54 +0000

    Update function with API fetch

--- T34/content_cats.js
+++ T34/content_cats.js
@@ -1,33 +1,10 @@
-// The group that writes all your learning content is fondly known as the
-// “content cats”. Because of this, it’d be a no-brainer to have you create a
-// website that will randomly generate GIFs of cats!
-// ○ Use the following API URL to fetch a random gif of a cat:
-// http://thecatapi.com/api/images/get?format=src&type=gif
-// ○ Ensure that you make use of async/await and not a promise
-// when fetching the API.
-// ○ Please only output the image URL in the console.
+// Fetch url of a random GIF of a cat
+async function catApiFunction() {
+  let item = await fetch(
+    "http://thecatapi.com/api/images/get?format=src&type=gif"
+  );
+  console.log(item.url); // log url of a gif
+}
 
-// async function apiFunctionContentCats() {
-//   let cat = await fetch(
-//     "http://thecatapi.com/api/images/get?format=src&type=gif"
-//   );
-//   console.log(cat.url);
-// }
-
-// apiFunctionContentCats();
-
-// console.log("hello");
-
-// async function apiFunction() {
-//     let response = await fetch("http://thecatapi.com/api/images/get?format=src&type=gif");
-//     console.log(response)
-//     console.log("---------------------")
-//     let url = await response.json();
-//     console.log(url)
-//     }
-
-fetch("http://thecatapi.com/api/images/get?format=src&type=gif")
-  .then(response => console.log(response[0].url));
-  (error) => {
-    console.log(error);
-  };
+// Call the function
+catApiFunction();
