f3244c0318389278dda5097656bccc3416620309
Kaja Jaronska <kaja.jaronska@gmail.com>
2023-01-04 22:37:23 +0000

    Add initial file for the Capstone Project outlining pseudocode

---
+++ T35/city_api.js
@@ -0,0 +1,6 @@
+// Pseudocode:
+// Create prompt for the user to enter name of the city | For development purposes create a variable with a static city name i.e. "johannesburg"
+// Create variables to hold information requested using API: population (GeoDB API), elevation (GeoDB API), currentTemperature (weather API)
+// Fetch details from the GeoDB API and assign to population and elevation variables; handle error
+// Fetch details from the weather API and assign to currentTemperature variable
+// Log the output for the user
