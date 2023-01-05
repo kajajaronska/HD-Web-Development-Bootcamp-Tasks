f1ce452274978bc8ffbf59c04d8cb89e51d35ae5
Kaja Jaronska <kaja.jaronska@gmail.com>
2023-01-05 20:18:32 +0000

    Add variable to hold user's city of choice

--- T35/city_api.js
+++ T35/city_api.js
@@ -1,6 +1,13 @@
 // Pseudocode:
 // Create prompt for the user to enter name of the city | For development purposes create a variable with a static city name i.e. "johannesburg"
+
+// let userCityChoice = prompt("Please enter name of the city:")
+
+let userCityChoice = "Johannesburg";
+
 // Create variables to hold information requested using API: population (GeoDB API), elevation (GeoDB API), currentTemperature (weather API)
 // Fetch details from the GeoDB API and assign to population and elevation variables; handle error
 // Fetch details from the weather API and assign to currentTemperature variable
 // Log the output for the user
+
+
