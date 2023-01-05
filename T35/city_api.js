73214bac016a4284520e041170c52d7c0a003873
Kaja Jaronska <kaja.jaronska@gmail.com>
2023-01-05 20:44:02 +0000

    Remove standalone variables for API results Create object with empty keys to store information from the API fetch Add fetch request and update created object with required information

--- T35/city_api.js
+++ T35/city_api.js
@@ -1,15 +1,44 @@
 // Pseudocode:
-// Create prompt for the user to enter name of the city | For development purposes create a variable with a static city name i.e. "johannesburg"
+// Create prompt for the user to enter name of the city | For development purposes create a variable with a static city name i.e. ""Aixovall". 
+// Free plan on RapidAPI limits result to 5 therefore I chose one of the results as a userCityChoice
 
 // let userCityChoice = prompt("Please enter name of the city:")
 
-let userCityChoice = "Johannesburg";
+let userCityChoice = "Andorra la Vella";
 
 // Create variables to hold information requested using API: population (GeoDB API), elevation (GeoDB API), currentTemperature (weather API)
 
-let population, elevation, currentTemperature;
+let cityResults = {
+    city_name: '',
+    city_ID: '',
+    population: '',
+    elevation: '',
+    currentTemperature: ''
+}
 
 // Fetch details from the GeoDB API and assign to population and elevation variables; handle error
+
+// const fetch = require('node-fetch');
+
+const url = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
+
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '9455bd8bbfmshca9af748330598fp1d0af3jsn6c0a653f823f',
+    'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+  }
+};
+
+fetch(url, options)
+	.then(res => res.json())
+	.then(json => {
+        cityResults.city_name = json.data[0].city;
+        console.log(json)})
+	.catch(err => console.error('error:' + err));
+
+
+
 // Fetch details from the weather API and assign to currentTemperature variable
 // Log the output for the user
 
