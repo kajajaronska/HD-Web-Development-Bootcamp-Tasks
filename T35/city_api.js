f37bc47b6c3bc757bb1d983d5e74d8483fe5c94b
Kaja Jaronska <kaja.jaronska@gmail.com>
2023-01-05 20:20:41 +0000

    Add variables to hold relevant information requested via API

--- T35/city_api.js
+++ T35/city_api.js
@@ -6,6 +6,9 @@
 let userCityChoice = "Johannesburg";
 
 // Create variables to hold information requested using API: population (GeoDB API), elevation (GeoDB API), currentTemperature (weather API)
+
+let population, elevation, currentTemperature;
+
 // Fetch details from the GeoDB API and assign to population and elevation variables; handle error
 // Fetch details from the weather API and assign to currentTemperature variable
 // Log the output for the user
