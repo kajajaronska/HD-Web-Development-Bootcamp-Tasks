614ee14b2f86a2c9a33a2ee9180022de64be6c2f
Kaja Jaronska <kaja.jaronska@gmail.com>
2023-01-09 21:38:22 +0000

    Refactor the code to make use of async/await while making fetch requests

--- T35/city_api.js
+++ T35/city_api.js
@@ -2,61 +2,66 @@
 // ID: 123214
 
 let city = {
-  city_name: 'Johannesburg',
-  ID: '131204',
-  population: '',
-  elevation: '',
-  latitude: '',
-  longitude: '',
-  currentTemperature: ''
-}
-
-// hard code city name and city ID (google)
-// change second API to the weather API or delay second request?? 
-
-let cityName = "New York City";
-
-// API details including key
-// const url1 = `https://wft-geo-db.p.rapidapi.com/v1/geo/cities?namePrefix=${cityName}`;
-
-const urlGeoDB = `https://wft-geo-db.p.rapidapi.com/v1/geo/cities/${city.ID}`;
-
-const options = {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Key': '9455bd8bbfmshca9af748330598fp1d0af3jsn6c0a653f823f',
-    'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-  }
+  city_name: "Johannesburg",
+  ID: "131204",
+  population: "",
+  elevation: "",
+  latitude: "",
+  longitude: "",
+  currentTemperature: "",
 };
 
 
+// let cityName = "New York City";
 
-// Create a function with API requests
+// API details including key
+
+let urlGeoDB = `https://wft-geo-db.p.rapidapi.com/v1/geo/cities/${city.ID}`;
+let urlWeather = `https://weatherbit-v1-mashape.p.rapidapi.com/current?lon=${city.longitude}&lat=${city.latitude}`;
+
+const optionsGeoDB = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "9455bd8bbfmshca9af748330598fp1d0af3jsn6c0a653f823f",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+const optionsWeather = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "9455bd8bbfmshca9af748330598fp1d0af3jsn6c0a653f823f",
+    "X-RapidAPI-Host": "weatherbit-v1-mashape.p.rapidapi.com",
+  },
+};
+
+// Create async function with API requests 
+// ADD TRY AND CATCH TO CAPTURE ERRORS
 
 async function fetchCityDetails() {
-  const[cityDetailsResponse, cityWeather] = await Promise.all([
-    fetch(urlGeoDB,options),
-    // fetch(url2,options)
-    
-  ])
-
+  const cityDetailsResponse = await fetch(urlGeoDB, optionsGeoDB);
   const cityDetails = await cityDetailsResponse.json();
-  // const cityDetails2 = await cityDetailsResponse2.json();
-  
-  return [cityDetails];
+
+  city.population = cityDetails.data.population;
+  city.elevation = cityDetails.data.elevationMeters;
+  city.latitude = cityDetails.data.latitude.toString();
+  city.longitude = cityDetails.data.longitude.toString();
+
+  urlWeather = `https://weatherbit-v1-mashape.p.rapidapi.com/current?lon=${city.longitude}&lat=${city.latitude}`;
+
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+
+  console.log("whoop");
+  console.log(city);
+
+  const weatherDetailsResponse = await fetch(urlWeather, optionsWeather);
+  const weatherDetails = await weatherDetailsResponse.json();
+
+  console.log(weatherDetails);
+
 }
 
-fetchCityDetails().then(([cityDetails]) => {
-
-    city.population = cityDetails.data.population;
-    city.elevation = cityDetails.data.elevationMeters;
-    city.latitude = cityDetails.data.latitude;
-    city.longitude = cityDetails.data.longitude;
-  
-  console.log(cityDetails);
-}).catch(err => console.error('error:' + err));
+fetchCityDetails();
 
 
-console.log(city)
-
-
+console.log(city);
